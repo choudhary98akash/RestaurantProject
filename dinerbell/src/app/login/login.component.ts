7210f01c7566c366774e429a17f67ea518b9013e
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-25 23:29:02 +0530

    landing page done

--- dinerbell/src/app/login/login.component.ts
+++ dinerbell/src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FormPageComponent } from './form-page/form-page.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormPageComponent,LandingPageComponent,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login = false;
+
+  showLogin(){
+    this.login = true;
+  }
+
+
 
 }
