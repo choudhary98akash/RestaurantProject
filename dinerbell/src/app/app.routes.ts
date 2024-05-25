dccd261b4ece6863c974cee7b1b4d9b2cb6654a0
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-25 20:53:21 +0530

    all componets done

--- dinerbell/src/app/app.routes.ts
+++ dinerbell/src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {path : '', component : LoginComponent},
-    {path : '/home', component : HomeComponent}
+    {path : 'home', component : HomeComponent}
 ];
