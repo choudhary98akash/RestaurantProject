ae1aca56799a1a646cebee457ea2acb6d7e0c8f4
Akash Choudhary <choudhary98akash@gmail.com>
2024-05-26 11:35:01 +0530

    added restaurant

--- dinerbell/src/app/home/restaurant/restaurant.component.ts
+++ dinerbell/src/app/home/restaurant/restaurant.component.ts
@@ -9,5 +9,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './restaurant.component.css'
 })
 export class RestaurantComponent {
+  
+  menuData =  {
+    restaurantName: "Turkish Delight",
+    imageUrl: "../../../assets/images/ali-inay-y3aP9oo9Pjc-unsplash.jpg", // Ensure this path is correct
+    menu: {
+      coldAppetizers: [
+        { name: "Humus", price: "₺" },
+        { name: "Cacık", price: "₺" }
+      ],
+      hotAppetizers: [
+        { name: "Sigara Börek", description: "Cigar Borek - Thin Pastry", price: "₺" }
+      ],
+      mainCourses: [
+        { name: "Adana Kebap", price: "₺" },
+        { name: "Tavuk Döner", description: "Chicken Döner", price: "₺" },
+        { name: "Pide Kaşarlı", description: "Pide with Cheese", price: "₺" }
+      ],
+      desserts: [
+        { name: "Baklava", price: "₺" }
+      ],
+      drinks: [
+        { name: "Türk Kahvesi", price: "₺" },
+        { name: "Ayran", price: "₺" }
+      ]
+    }
+  };
+
 
 }
